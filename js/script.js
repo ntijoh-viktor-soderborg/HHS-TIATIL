c092b6d357eb00ba0e8c4ecb8e2d22e17936a8e6
viktor-soderborg-nti-johanneberg <74763822+viktor-soderborg-nti-johanneberg@users.noreply.github.com>
2022-03-17 16:29:31 +0100

    Game Loop

--- js/script.js
+++ js/script.js
@@ -1,38 +1,47 @@
 let order = [];
-let test1 = document.querySelector("main h2.test1");
-let test2 = document.querySelector("main h2.test2");
-let test3 = document.querySelector("main h2.test3");
-let player1 = 'v';
-let player2 = 'b';
-let player3 = 'n';
-
 let songs = ['TheFatRat - Fly Away feat. Anjulie.mp3', 'TheFatRat - Monody (feat. Laura Brehm).mp3','TheFatRat - Unity.mp3', 'TheFatRat & RIELL - Hiding In The Blue [Chapter 1].mp3']
 let delays = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
 
 let audio = document.querySelector('audio');
 
-document.body.addEventListener('keydown', (e) => {
-    // order.push(e.key);
-    // updateUI(order);
-    if (e.key === 'p') {
-        let uwu = setInterval(circkle, 10);
-        let stopTime = Math.floor(Math.random() * delays.length + 1) * 2000;
-        let songIndex = Math.floor(Math.random() * songs.length);
-        let randomSong = new Audio(`./assets/${songs[songIndex]}`);
-        document.querySelector('h1').innerText = `Currently playing: ${songs[songIndex].slice(0,-4)}`
-        playSong(randomSong);
-        console.log(`Song stop time = ${stopTime}`)
-        setTimeout(() => {
-            players--
-            clearInterval(uwu)
-            stopSong(randomSong);
-        }, stopTime)
-    }
-})
+window.onload = (event) => {
+    game();
+}
+
+function game() {
+    order = [];
+    let playTime = setInterval(circkle, 10);
+    let stopTime = Math.floor(Math.random() * delays.length + 1) * 2000;
+    let songIndex = Math.floor(Math.random() * songs.length);
+    let randomSong = new Audio(`./assets/${songs[songIndex]}`);
+    document.querySelector('h1').innerText = `Currently playing: ${songs[songIndex].slice(0,-4)}`
+    playSong(randomSong);
+    console.log(`Song stop time = ${stopTime}`)
+    setTimeout(() => {
+        clearInterval(playTime)
+        stopSong(randomSong);
+        document.body.addEventListener('keydown', (e) => {
+            if (keys.includes(e.key) && !order.includes(e.key)) {
+                order.push(e.key);
+                updateUI(order);
+            }
+        })
+    }, stopTime)
+}
+
 
 function updateUI(list) {
-    for (let i = 0; i < list.length; i++) {
-        console.log(list[i])
+    console.log(list)
+    if (list.length === players) {
+        if (list.length === 1) {
+            console.log("hej")
+        } else {
+            let listDup = [...list]
+            let lastPlayer = listDup.pop()
+            //Remove lastPlayer by their key which is recived by function above
+            players--
+            game();
+        }
     }
 }
 
--- test/test.js
+++ test/test.js
@@ -1,40 +1,47 @@
-function player(x,y) {
-    ctx.beginPath();
-    ctx.arc(x, y, 15, 0, 2 * Math.PI);
-    ctx.fillStyle = getRndColor();
-    ctx.fill();
-}
+let playerNumb = 0;
+let keys = ['a', 's', 'd', 'f'];
+// ^ Vilka knappar som en spelare har tillgång till
+let keyNumb = -1;
 
-function Drawcirckle() {
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
-    ctx.beginPath();
-    ctx.arc(200, 200, 180, 0, 2 * Math.PI);
-    ctx.stroke();
-}
+// function player(x,y) {
+//     ctx.beginPath();
+//     ctx.arc(x, y, 15, 0, 2 * Math.PI);
+//     ctx.fillStyle = getRndColor();
+//     ctx.fill();
+// }
 
-function drawHand(ctx, pos, length, width) {
-    ctx.beginPath();
-    ctx.lineWidth = width;
-    ctx.lineCap = "round";
-    ctx.moveTo(0,0);
-    ctx.rotate(pos);
-    ctx.lineTo(0, -length);
-    ctx.stroke();
-    ctx.rotate(-pos);
-  }
+// function Drawcirckle() {
+//     var c = document.getElementById("myCanvas");
+//     var ctx = c.getContext("2d");
+//     ctx.beginPath();
+//     ctx.arc(200, 200, 180, 0, 2 * Math.PI);
+//     ctx.stroke();
+// }
+
+// function drawHand(ctx, pos, length, width) {
+//     ctx.beginPath();
+//     ctx.lineWidth = width;
+//     ctx.lineCap = "round";
+//     ctx.moveTo(0,0);
+//     ctx.rotate(pos);
+//     ctx.lineTo(0, -length);
+//     ctx.stroke();
+//     ctx.rotate(-pos);
+//   }
 
 function getRndColor() {
+    playerNumb++
+    keyNumb++
     var r = 255*Math.random()|0,
         g = 255*Math.random()|0,
         b = 255*Math.random()|0;
-    return 'rgb(' + r + ',' + g + ',' + b + ')';
+    return {name: `Player ${playerNumb}`, color:'rgb(' + r + ',' + g + ',' + b + ')', key:keys[keyNumb]};
 }
 
 function circkle() {
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
-    ctx.clearRect(0, 0, 400, 400); // clear canvas
+    ctx.clearRect(0, 0, 500, 500); // clear canvas
 
     var now = new Date();
     var milisecond = now.getMilliseconds();
@@ -54,7 +61,9 @@ function circkle() {
             ctx.beginPath();
             ctx.arc(a, b, 15, 0, 2 * Math.PI);
             // console.log(colors[i])
-            ctx.fillStyle = colors[i];
+            ctx.fillStyle = colors[i]["color"];
+            ctx.font = "20px Arial";
+            ctx.fillText(`${colors[i]["name"]}`, a-35, b-20);
             ctx.fill();
         } else {
             // console.log(degstart+1)
@@ -63,16 +72,18 @@ function circkle() {
             // console.log(x,y)
             ctx.beginPath();
             ctx.arc(x, y, 15, 0, 2 * Math.PI);
-            ctx.fillStyle = colors[i];
-            ctx.fill();  
+            ctx.fillStyle = colors[i]["color"];
+            ctx.font = '20px Arial';
+            ctx.fillText(`${colors[i]["name"]}`, x-35, y-20);
+            ctx.fill();
         }
     }
     count = count + 0.01
     ctx.save()
 }
 
-let colors = []
-let players= document.location["href"].split("=")[1]
+let colors = [];
+let players= parseInt(document.location["href"].split("=")[1]);
 
 // console.log(formData)
 for (let i = 0; i < players; i++) {
